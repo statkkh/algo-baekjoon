8d727de73c65aad0aa85bdb010e4899e1832cb4e
statkkh <140682514+statkkh@users.noreply.github.com>
2023-10-15 18:44:14 +0900

    2526 BOJ

---
+++ 2526.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/2526
+#include <bits/stdc++.h>
+using namespace std;
+int n,p,ne,cnt=1;
+vector <int> nums(1001); 
+
+int main(){
+    cin >> n >> p;
+    ne = n;
+    while(!nums[ne]){
+        nums[ne] = cnt++;
+        ne = ne * n % p;
+    }
+    cout << cnt - nums[ne] << '\n';
+}
