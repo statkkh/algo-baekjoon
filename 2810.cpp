8a40b956274203514dea38692e3eec54656182f7
statkkh <140682514+statkkh@users.noreply.github.com>
2023-10-15 09:28:14 +0900

    bronze -1

---
+++ 2810.cpp
@@ -0,0 +1,30 @@
+// https://solved.ac/problems/level/5 
+// https://www.acmicpc.net/problem/2810
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int n;
+char c;
+bool flag;
+double sum;
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> c;
+		if (c == 'S') sum++;
+		else //(c == 'L')
+		{
+			flag = true;
+			sum += 0.5;
+		}	
+	}
+	
+	if (flag) cout << sum + 1;
+	else cout << sum;
+
+	
