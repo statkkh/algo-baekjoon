a77be43051452b29c3ee57b665535e7082d6f9fb
statkkh <140682514+statkkh@users.noreply.github.com>
2023-10-26 08:54:18 +0900

    BOJ 1924 bronze-1 수정

--- 1924.cpp
+++ 1924.cpp
@@ -1,17 +1,24 @@
+// https://www.acmicpc.net/problem/1924
 #include <bits/stdc++.h>
 #include <vector>
 using namespace std;
 int month, day,totalday;
 bool present;
+string days[7] = {"MON", "TUE","WED","THU","FRI", "SAT", "SUN"}; 
+int monthlist[6] = {1, 3, 5, 7, 8, 10};//31
+
 int main() {
     ios_base::sync_with_stdio(false);
 
     scanf("%d %d", &month, &day);
 
-    string days[7] = {"MON", "TUE","WED","THU","FRI", "SAT", "SUN"};
-    int monthlist[6] = {1, 3, 5, 7, 8, 10}; //31   
-       
     totalday += (day-1);
+
+    if(month == 1){
+        day %= 7;
+        cout << days[day];
+        return;
+    }
     
     for(int i = 1;  i < month ; i++){
         present = count(begin(monthlist), end(monthlist), i) > 0;
